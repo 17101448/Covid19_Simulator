aa73d717c04477588779aa2400ce8da7e529f362
17101448 <the_1_week@naver.com>
2021-11-23 13:41:11 +0900

    git commit

---
+++ Vaccine.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class Vaccine {
+
+    private Person[][] people; 
+    private int size = 25;
+    private int[] reanOrder;
+
+    private List<Integer[]> history;
+
+    public Vaccine(int size){}
+    
+
+    public Vaccine(int size, double vaccineRatio){}
+
+    private void initialInfection(){}
+
+    public void vaccinate(double vaccineRatio){}
+
+    private int[] randPerm(int n){}
+
+    public void step(double infectionRate, double recoveryRate){}
+
+    public Integer[] countStates(){}
+
+    public State[][] getPeopleState(){}
+
+    public List<Integer[]> getHistory(){
+
+    public void printStep(int n){}
+    }
+    
+}
